c466bd73cab288f97dbb20dd949f54083784ff4e
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-11 12:28:47 +0200

    Something

--- src/index.js
+++ src/index.js
@@ -12,5 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('hozzad').addEventListener('click', () => {
         let szam = parseInt(document.getElementById('szam').value);
         lista.push(szam);
-    });  
+    }); 
 });
+
+class Szazlabu{
+    #labakszama;
+    Szazlabu(labakszama){
+        this.labakszama = labakszama;
+    }
+    toString(){
+        return this.labakszama+" lábú százlábú";
+    }
+}
+function labakbolSzazlabuk(lista){
+    return lista.map(e => new Szazlabu(e));
+}

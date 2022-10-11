56c58632bc501084db237ee5ea61a03379db79da
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-11 11:08:01 +0200

    Feladat 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 let lista = [];
 let oteloszt = [];
+for (let i = 0; i < 20; i++) {
+    lista.push(Math.floor(Math.random() * 30) + 1)
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    for (let i = 0; i < 20; i++) {
-        lista.push(Math.floor(Math.random() * 30) + 1)
-    }
+    document.getElementById('kivalogat').addEventListener('click', () => {
+
     oteloszt = lista.filter(e => e%5==0);
     for (let i = 0; i < oteloszt.length; i++) {
         console.log(oteloszt[i]);
     }
+    });
 });
